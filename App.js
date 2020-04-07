44bbd3656dae2dcd3e891b08f4cc258076dc83ec
Viswanath.Ct <2ndryid@gmail.com>
2020-04-07 10:35:19 +0530

    Built a rudimentary example.

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import native from "declair/native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const Declair = native({
+		providers: {},
+		types: {
+			element: {
+				config: {
+					style: typeStyles.element,
+				},
+			},
+		},
+	});
+
+	return <Declair {...{
+		data: {
+			message: 'Some text passed as data from the parent!',
+		},
+		items: {
+			child: {
+				items: {
+					message: {
+						type: 'text',
+						data: 'Some text as embedded data!',
+					}
+				},
+			},
+			message: {
+				type: 'text',
+			}
+		},
+		style: styles.element,
+	}}/>
 }
 
+const typeStyles = StyleSheet.create({
+  element: {
+		backgroundColor: '#ddd',
+		borderWidth: 4,
+		alignItems: 'center',
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+	},
+});
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  element: {
+		borderColor: '#f00',
+		borderWidth: 4,
+	},
+	text: {
+		fontSize: 10,
   },
 });
