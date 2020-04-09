78a709777c78536bf9c7716d10e4e419e2072a74
Viswanath.Ct <2ndryid@gmail.com>
2020-04-09 13:14:57 +0530

    Streamlined with the changes in declair.

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import native from "declair/native";
+import { StyleSheet } from 'react-native';
+import declair from "declair/quick";
 
 export default function App() {
-	const Declair = native({
-		providers: {},
+	const Declair = declair({
 		types: {
 			element: {
 				config: {
